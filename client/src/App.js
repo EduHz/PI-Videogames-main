631c1a024bff37b54e490f75c179c4eebbcba2f5
EduHz <106116546+EduHz@users.noreply.github.com>
2022-12-02 21:47:41 -0300

    Cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
+   <Card />
   );
 }
 
---
+++ client/src/components/Card.jsx
@@ -0,0 +1,32 @@
+import './Card.css'
+
+export default function Card() {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="imgBx">
+          <img src="https://i.blogs.es/dfbccc/trucosgtavps4/1366_2000.jpg" />
+        </div>
+        <div className="contentBx">
+          <h2>Gta V</h2>
+          
+          <div className="size">
+            <h3>Platforms :</h3>
+            <span>PC</span>
+            <span>XBOX</span>
+            <span>PS5</span>
+            
+          </div>
+          <div className="color">
+            <h3>Genero :</h3>
+            <span></span>
+            <span></span>
+            <span></span>
+            
+          </div>
+          <a href="#">DETAIL</a>
+        </div>
+      </div>
+    </div>
+  );
+}
