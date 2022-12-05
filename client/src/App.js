e375b05b44782dfc0e7ebe1b70a66a84f48b4804
EduHz <106116546+EduHz@users.noreply.github.com>
2022-12-05 16:45:07 -0300

    go

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import Home from "./components/Home/Home.jsx"
+import Home from "./components/Home/Home.jsx";
 import LandingPage from "./components/LandingPage/LandingPage.jsx";
 import { Route } from "react-router-dom";
 import About from "./components/About/About.jsx";
-import CreateVideogame from './components/CreateVideogame/CreateVideogame.jsx'
-import DetailVideogame from './components/DetailVideogame/DetailVideogame'
+import CreateVideogame from "./components/CreateVideogame/CreateVideogame.jsx";
+import DetailVideogame from "./components/DetailVideogame/DetailVideogame";
 
 function App() {
   return (
@@ -15,13 +15,13 @@ function App() {
         <Home />
       </Route>
       <Route exact path="/create">
-      <CreateVideogame />
+        <CreateVideogame />
       </Route>
       <Route exact path="/about">
         <About />
       </Route>
       <Route exact path="/videogame/:id">
-      <DetailVideogame />
+        <DetailVideogame />
       </Route>
     </>
   );
