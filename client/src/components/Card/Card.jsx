e1d1437e1aaf15b837da224503e4772275e6419c
Edu <eduhzprogramador@gmail.com>
2022-12-14 13:18:38 -0300

    card detail

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,24 +1,27 @@
 import "./Card.css";
+import { Link } from "react-router-dom";
 
 export function Card({ games }) {
   return (
     <div className="container">
       {games.map((res, index) => (
         <div className="card" key={index}>
-          <img
-            className="card-img"
-            src={res.background_image}
-            alt={res.name}
-            title={res.name}
-          />
-          <div className="text">
-            <h3>{res.name.slice(0, 20)}</h3>
-            <h4>
-              {/*De esta forma mostramos unicamente 3 generos*/}
-              {res.genres.map((x) => x.name + " ").slice(0, 2)}
-              <br />⭐{res.rating}
-            </h4>
+          <Link to={`detail/id`}>
+            <img
+              className="card-img"
+              src={res.background_image}
+              alt={res.name}
+              title={res.name}
+            />
+            <div className="text">
+              <h3>{res.name.slice(0, 20)}</h3>
+              <h4>
+                {/*De esta forma mostramos unicamente 3 generos*/}
+                {res.genres.map((x) => x.name + " ").slice(0, 2)}
+                <br />⭐{res.rating}
+              </h4>
           </div>
+          </Link>
         </div>
       ))}
     </div>
