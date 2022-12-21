ccc2b57482ce2ac4d42b927135de1cb8de418666
Edu <eduhzprogramador@gmail.com>
2022-12-21 20:35:21 -0300

    home finished

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,29 +1,22 @@
 import "./Card.css";
 import { Link } from "react-router-dom";
 
-export function Card({ games }) {
+export function Card({ name, rating, image, genres, id }) {
   return (
     <div className="container">
-      {games.map((res, index) => (
-        <div className="card" key={index}>
-          <Link to={`detail/${res.id}`}>
-            <img
-              className="card-img"
-              src={res.background_image}
-              alt={res.name}
-              title={res.name}
-            />
-          </Link>
-          <div className="text">
-            <h3>{res.name.slice(0, 20)}</h3>
-            <h4>
-              {/*De esta forma mostramos unicamente 3 generos*/}
-              {res.genres.map((x) => x.name + " ").slice(0, 2)}
-              <br />⭐{res.rating}
-            </h4>
-          </div>
+      <div className="card" key={id}>
+        <Link to={`detail/${id}`}>
+          <img className="card-img" src={image} alt={name} title={name} />
+        </Link>
+        <div className="text">
+          <h3>{name.slice(0, 20)}</h3>
+          <h4>
+            {/*De esta forma mostramos unicamente 3 generos*/}
+            {genres}
+            <br />⭐{rating}
+          </h4>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,55 +3,92 @@ import Nav from "../Nav/Nav";
 import Pagination from "../Pagination/Pagination";
 import { Card } from "../Card/Card";
 import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getVideogames } from "../../redux/actions";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  const [games, setGames] = useState([]);
-  //Necistamos almacenar el objeto de info para la paginacion
-  const [info, setInfo] = useState({});
+  let estadoGames = useSelector((state) => state.videogames);
 
-  const initialUrl =
-    "https://api.rawg.io/api/games?key=2ebf403c8d3847529cb6010c7dc76678&page=2";
+  const dispatch = useDispatch();
 
-  //Funcion para hacer una request al Link de la API
-  const fetchGames = (url) => {
-    fetch(url)
-      //La funcion fetch retorna una promesa
-      .then((respuesta) => respuesta.json())
-      //con respuesta.json, lo convertimos en una estructura JavaScript
-      .then((datosDeLaApi) => {
-        setGames(datosDeLaApi.results);
-        setInfo(datosDeLaApi);
-      })
-      //datosDeLaApi, son los datos de la api, para poder consumir en JavaScript
-      .catch((error) => console.log(error));
-  };
-
-  function onPreviuos() {
-    fetchGames(info.previous);
-  }
-  //Debemos pasarles estas funciones al componente de "pagination"
-  function onNext() {
-    fetchGames(info.next);
-  }
-
-  //UseEffect , nos permite manejar los efectos colaterales
   useEffect(() => {
-    //Llamamos a la funcion, y la damos el parametro deseado (initialUrl)
-    fetchGames(initialUrl);
-  }, []);
+    dispatch(getVideogames());
+  }, [dispatch]);
+
+  console.log(estadoGames);
 
   return (
     <>
       <div className="home">
         <Nav />
-        <Card games={games} />
-        <Pagination
-          prev={info.prev}
-          next={info.next}
-          onPreviuos={onPreviuos}
-          onNext={onNext}
-        />
+        {estadoGames.length > 0 ? (
+          estadoGames.map((game) => (
+            <Card
+              
+              key={game.id}
+              name={game.name}
+              rating={game.rating}
+              image={game.image}
+              genres={game.genres}
+            />
+          ))
+        ) : (
+          <h2>No hay nada</h2>
+        )}
       </div>
     </>
   );
 }
+//   const [games, setGames] = useState([]);
+//   //Necistamos almacenar el objeto de info para la paginacion
+//   const [info, setInfo] = useState({});
+
+//   const initialUrl =
+//     "https://api.rawg.io/api/games?key=2ebf403c8d3847529cb6010c7dc76678&page=2";
+
+//   //Funcion para hacer una request al Link de la API
+//   const fetchGames = (url) => {
+//     fetch(url)
+//       //La funcion fetch retorna una promesa
+//       .then((respuesta) => respuesta.json())
+//       //con respuesta.json, lo convertimos en una estructura JavaScript
+//       .then((datosDeLaApi) => {
+//         setGames(datosDeLaApi.results);
+//         setInfo(datosDeLaApi);
+//       })
+//       //datosDeLaApi, son los datos de la api, para poder consumir en JavaScript
+//       .catch((error) => console.log(error));
+//   };
+
+//   function onPreviuos() {
+//     fetchGames(info.previous);
+//   }
+//   //Debemos pasarles estas funciones al componente de "pagination"
+//   function onNext() {
+//     fetchGames(info.next);
+//   }
+
+//   //UseEffect , nos permite manejar los efectos colaterales
+//   useEffect(() => {
+//     //Llamamos a la funcion, y la damos el parametro deseado (initialUrl)
+//     fetchGames(initialUrl);
+//   }, []);
+
+//   return (
+//     <>
+//       <div className="home">
+//         <Nav />
+//         <Card games={games} />
+//         <Pagination
+//           prev={info.prev}
+//           next={info.next}
+//           onPreviuos={onPreviuos}
+//           onNext={onNext}
+//         />
+//       </div>
+//     </>
+//   );
+// }
+
+//!video minuto 43:50
