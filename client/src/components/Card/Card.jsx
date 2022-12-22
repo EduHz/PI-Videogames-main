83a1cec4d976a1dabeb3f7f8380db20437e0c295
Edu <eduhzprogramador@gmail.com>
2022-12-22 17:33:55 -0300

    Detail 90%

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -5,14 +5,14 @@ export function Card({ name, rating, image, genres, id }) {
   return (
     <div className="container">
       <div className="card" key={id}>
-        <Link to={`detail/${id}`}> 
+        <Link to={`detail/${id}`}>
           <img className="card-img" src={image} alt={name} title={name} />
         </Link>
         <div className="text">
           <h3>{name.slice(0, 20)}</h3>
           <h4>
             {/*De esta forma mostramos unicamente 3 generos*/}
-            {genres}
+            {genres.split(",").slice(0, 2)}
             <br />⭐{rating}
           </h4>
         </div>
--- client/src/components/DetailVideogame/DetailVideogame.jsx
+++ client/src/components/DetailVideogame/DetailVideogame.jsx
@@ -15,47 +15,27 @@ export default function DetailVideogame() {
     dispatch(getVideogameById(idPage));
   }, []);
 
-  console.log(game);
-
   return (
     <>
       <Nav />
       <div className="container-detail">
         <div className="card-detail">
-          <img
-            className="detail-img"
-            src="https://images3.alphacoders.com/274/274397.jpg"
-            alt="demo"
-          />
+          <img className="detail-img" src={game.image} alt="demo" />
           <h3>{game.name}</h3>
           <h4>
-            Genres : Action - Adventure
+            Genres <br />
+            ◽{game.genres}
             <br />
-            Rating : 4.3
+            Rating<br /> ◽{game.rating}
             <br />
-            Release date : 10/2/2010
+            Release Date<br /> ◽{game.released}
             <br />
-            Plataforms : PS3 - PC
+            <div className="platforms">
+              Plataforms<br />
+              ◽{game.platforms}
+            </div>
           </h4>
-          <p className="p-detail">
-            Rockstar Games went bigger, since their previous installment of the
-            series. You get the complicated and realistic world-building from
-            Liberty City of GTA4 in the setting of lively and diverse Los
-            Santos, from an old fan favorite GTA San Andreas. 561 different
-            vehicles (including every transport you can operate) and the amount
-            is rising with every update. <br />
-            Simultaneous storytelling from three unique perspectives: <br />
-            Follow Michael, ex-criminal living his life of leisure away from the
-            past, Franklin, a kid that seeks the better future, and Trevor, the
-            exact past Michael is trying to run away from. <br />
-            GTA Online will provide a lot of additional challenge even for the
-            experienced players, coming fresh from the story mode. Now you will
-            have other players around that can help you just as likely as ruin
-            your mission. Every GTA mechanic up to date can be experienced by
-            players through the unique customizable character, and community
-            content paired with the leveling system tends to keep everyone busy
-            and engaged.
-          </p>
+          <div className="p-detail">{game.description}</div>
         </div>
         <img className="samus" src={logo} alt="" />
       </div>
