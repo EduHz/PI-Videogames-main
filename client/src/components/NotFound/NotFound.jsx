ae013046b707cafee67b691fe6a5f90986f3b738
eduhz <edugzprogramador@gmail.com>
2023-01-06 00:18:29 -0300

    notFound

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,10 +1,16 @@
 import Nav from '../Nav/Nav'
+import logo from '../../assets/not-found.png'
+import '../NotFound/NotFound.css'
 
 export default function NotFound() {
   return (
     <>
       <Nav/>
-      <h1>No hay nada papa</h1>
+      <div className='container-notfound'>
+        <img src={logo} className="image-notfound" alt="imagen notFound" />
+        <h1>Site Not Found</h1>
+        <h3>Well, this is akward. The site you're looking for is not here</h3>
+      </div>
     </>
   );
 }
