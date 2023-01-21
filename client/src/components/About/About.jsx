e1fe8762979ee77db357c2836ed4b2de60b68ebb
Eduardo <eduhzprogramador@gmail.com>
2023-01-21 16:19:06 -0300

    about changues

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,10 +1,53 @@
 import Nav from "../Nav/Nav";
+import "./About.css";
 
 export default function About() {
   return (
-    <div>
+    <>
       <Nav />
-      <h2>Soy el compo About</h2>
-    </div>
+      <div className="about-container">
+        <h2 className="h2-about">Hi, this is my Individual Project</h2>
+        <h4 className="about-h4">
+          This project is part of the final stage of my studies at the Henry
+          Academy. In my case, the project I had to carry out was a video game
+          website. On this page you can filter the video games, create new video
+          games, and see the details of each one of them <br /> Here I use all
+          the technologies learned:
+        </h4>
+        <div className="list-about">
+          <li>Javascript</li>
+          <br />
+          <li>React</li>
+          <br />
+          <li>React-Redux</li>
+          <br />
+          <li>React-Router</li>
+          <br />
+          <li>Postgresql</li>
+          <br />
+          <li>Express</li>
+          <br />
+          <li>Sequalize</li>
+          <br />
+          <li>Morgan</li>
+        </div>
+        <h3>I hope you enjoy it 👍</h3>
+      </div>
+      <div className="contact-container">
+        <h5>Contact to me</h5>
+        <a href="https://www.linkedin.com/in/edu-hz/" target="_blank">
+          <img src="https://img.icons8.com/glyph-neue/50/null/linkedin-circled.png" />
+        </a>
+        <a href="https://wa.me/541123989941" target="_blank">
+          <img src="https://img.icons8.com/ios-filled/45/null/whatsapp--v1.png" />
+        </a>
+        <a href="https://github.com/EduHz">
+          <img src="https://img.icons8.com/sf-black-filled/60/null/github.png" />
+        </a>
+      </div>
+    </>
   );
 }
+
+// -Javascript -React -React-redux -React-router -Postgresql -Express
+//         -Sequalize -Morgan
