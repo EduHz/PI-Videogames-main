5ca021df992d5f0d0617f88bd4c50c1cc0b3cd53
EduHz <eduhzprogramador@gmail.com>
2023-02-03 15:49:26 -0300

    new try

--- api/index.js
+++ api/index.js
@@ -17,14 +17,12 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require("./src/app.js");
-const { conn } = require("./src/db.js");
-require("dotenv").config();
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log("%s listening at", process.env.PORT); // eslint-disable-line no-console
+  server.listen(3001, () => {
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
- 
--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
-
-// axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
-
-
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
