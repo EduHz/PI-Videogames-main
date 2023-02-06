27b85822e94d7825fe524cd1e4d5408468de84ce
EduHz <eduhzprogramador@gmail.com>
2023-02-06 17:24:01 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,14 +17,14 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-require('dotenv').config()
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+require("dotenv").config();
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log('%s listening at', process.env.PORT); // eslint-disable-line no-console
+    console.log("%s listening at", process.env.PORT); // eslint-disable-line no-console
   });
 });
 
