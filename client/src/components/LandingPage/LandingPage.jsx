73bb43a9737dc84d4831a6be195015d6cbfd9e4f
EduHz <eduhzprogramador@gmail.com>
2023-05-12 18:45:39 -0300

    prettier changues

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -5,22 +5,21 @@ import "./LandingPage.css";
 function LandingPage() {
   return (
     <>
-    <div className="mylandingpage">
-      <div className="container-landing">
-        <h1 className="h1-landing">VideoGames</h1>
-        <h4 className="h4-landing">
-          Welcome to my Henry Academy solo project. You can search for video
-          games and create your owm. Enjoy it
-          <span>&#160;</span>
-        </h4>
-        <Link to="/home" style={{ textDecoration: "none" }}>
-          <h4 className="parpadea">PRESS START</h4>
-        </Link>
+      <div className="mylandingpage">
+        <div className="container-landing">
+          <h1 className="h1-landing">VideoGames</h1>
+          <h4 className="h4-landing">
+            Welcome to my Henry Academy solo project. You can search for video
+            games and create your owm. Enjoy it
+            <span>&#160;</span>
+          </h4>
+          <Link to="/home" style={{ textDecoration: "none" }}>
+            <h4 className="parpadea">PRESS START</h4>
+          </Link>
+        </div>
       </div>
-    </div>
     </>
   );
 }
 
 export default LandingPage;
-
